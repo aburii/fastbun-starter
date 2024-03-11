9ec63f6e8cd97c22af6436ba0871cc5d5b23538b
Jolan <jolan.brizard@epitech.eu>
2024-03-11 17:06:37 +0100

    refactor: config
    + add scoped config files for lint and tsc
    - global and useless config files

--- apps/back/src/index.ts
+++ apps/back/src/index.ts
@@ -3,5 +3,5 @@ import { Elysia } from "elysia";
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3001);
 
 console.log(
-  `🔥 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🔥 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
--- apps/front/nuxt.config.ts
+++ apps/front/nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  
+
   typescript: {
     strict: true,
     shim: false,
   },
-})
+});
